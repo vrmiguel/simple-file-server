1455bbaa4f5610540414cb0d37578629895bbcac
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-02-18 13:19:51 -0300

    Remove comments

--- utils.c
+++ utils.c
@@ -2,13 +2,6 @@
 #include "ctype.h"
 #include "string.h"
 
-// On socket and datagram sockets:
-//  https://stackoverflow.com/questions/5815675/what-is-sock-dgram-and-sock-stream
-
-//struct addrinfo make_tcp_addr_info() {
-
-//};
-
 //!
 //! get_address returns either an IPv4 or an IPv6 address, depending on the passed sockaddr *.
 //!
@@ -32,25 +25,3 @@ char * rtrim(char *s)
     *(back+1) = '\0';
     return s;
 }
-
-//get_request_t get_file(const char * filename) {
-//    get_request_t file_request;
-//    FILE *file = fopen(filename, "rb");
-//    if (!file) {
-//        fprintf(stderr, "server: error: could not open file '%s'.\n", filename);
-//        file_request.status = 1; // We're adopting 1 as an error, 0 as OK.
-//        return file_request;
-//    }
-
-//    // Get size of file
-//    fseek(file, 0L, SEEK_END);
-//    size_t sz = ftell(file);
-//    rewind(file);
-
-//    file_request.contents = malloc(sz);
-//    fread(file_request.contents, sz, 1, file);
-//    file_request.size = sz;
-//    file_request.status = 0;
-
-//    return file_request;
-//}
--- utils.h
+++ utils.h
@@ -2,19 +2,9 @@
 #define UTILS_H
 #include <netdb.h>
 
-//struct get_request_s {
-//    char * contents;
-//    size_t size;
-//    unsigned char status;
-//};
-//typedef struct get_request_s get_request_t;
-
 typedef int fd_t;
 
-//struct
-
 char * rtrim(char *);
-
 void * get_address(struct sockaddr *);
 
 #endif // UTILS_H
